e05a9bd68fa2bf5419f29d4070b5af5d1544f5a7
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-10-01 20:04:39 +0100

    Added basic structure and apache restart command

---
+++ index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+'use strict';
+
+var inquirer = require("inquirer");
+var spawny = require("spawny");
+var chalk = require('chalk');
+
+var commands =  [
+    'Restart apache',
+    'Close Server Tools'
+];
+
+var shouldExecute = false;
+
+chooseCommand();
+
+function chooseCommand() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'command',
+      message: 'What would you like to do?',
+      choices: commands
+    }
+  ], function(answers) {
+    parseCommand(answers.command);
+  });
+}
+
+function parseCommand(command) {
+  if(command == 'Close Server Tools') {
+    return;
+  }
+
+  if(command == 'Restart apache') {
+    executeCommand('service apache2 restart')
+  }
+}
+
+function executeCommand(command) {
+  console.log("Executing command: " + chalk.cyan(command));
+  if(shouldExecute) {
+    spawny(command, true, chooseCommand);
+  } else {
+    chooseCommand();
+  }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Robinson College Boat Club Server Tools
+
+This is a command line application for helping administer the RCBC server.
+
+To install, run this on the server:
+
+`npm install -g robinsoncollegeboatclub/server-tools`
+
+To use, SSH into the server and then run `server-tools`
